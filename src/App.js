ccbf04fb57bb4a824da80483e70205377c939dd1
Abhishek Singhal <abhisinghal112001@gmail.com>
2022-12-05 22:05:01 +0530

    interface

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-import { Route, Routes,Navigate } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import './App.css';
 import Snake from "./component/Snake";
 import Main from "./Main";
 
 function App() {
 
- return(
-  <Routes className="main">
-    <Route path="/" element={<Main/>}/>
-    <Route path="/Snake" element={<Snake/>}/>
-    <Route path="*" element={<Navigate to={"/"} />} />
-  </Routes>
- )
+  return (
+    <>
+      {/* navbar */}
+      < nav >
+        <ul className="nav nav-pills">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href={"/GameHub"}>☠️ GameHub</a>
+          </li>
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" to={"/"} role="button" aria-expanded="false">🎮 Games</a>
+            <ul className="dropdown-menu">
+              <li><a className="dropdown-item" href={"/GameHub/Snake"}>🐍 Snake</a></li>
+              <li><a className="dropdown-item" to={"/"}>Another action</a></li>
+              <li><a className="dropdown-item" to={"/"}>Something else here</a></li>
+              <li><hr className="dropdown-divider" /></li>
+              <li><a className="dropdown-item" to={"/"}>Separated link</a></li>
+            </ul>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href={"https://github.com/Abhimrt"}>🧑🏻‍💻 Developer</a>
+          </li>
+         
+        </ul>
+      </nav >
+      <Routes className="main">
+        <Route path="/" element={<Main />} />
+        <Route path="/Snake" element={<Snake />} />
+        <Route path="*" element={<Navigate to={"/"} />} />
+      </Routes>
+    </>
+  )
 }
 
 export default App;
