6df0c0350dac1974031a89666bf039b5f6d7fcf8
Abhishek Singhal <abhisinghal112001@gmail.com>
2022-12-10 22:16:49 +0530

    f

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes, Navigate, NavLink } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import './App.css';
 import Snake from "./component/Snake";
 import Sudoku from "./component/Sudoku";
